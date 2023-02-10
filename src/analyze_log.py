bb889b826becf9f49f7647c926f0145219532e07
Leonan Costa <leonanfecosta@gmail.com>
2023-02-10 16:12:15 -0300

    feat: função favorite_order para verificar o prato mais pedido de um cliente

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,14 @@
+def favorite_order(name, orders):
+    count_orders = {}
+    for order in orders:
+        if order["name"] == name:
+            if order["dish"] not in count_orders:
+                count_orders[order["dish"]] = 1
+            else:
+                count_orders[order["dish"]] += 1
+
+    return max(count_orders, key=count_orders.get)
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
